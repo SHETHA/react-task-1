38eef376626409a5f2e074114cfcb4e7f0475c8e
SHETHA <shethabindhcpsalil@gmail.com>
2022-12-06 12:18:24 +0530

    react task 1 ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
-
+import vector from './Vector.svg';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="heading">
+        My Tasks
+      </div>
+      <div className="inputbar">
+        <div className="inputText">
+          <input type="text" placeholder="Add new task"/>
+        </div>
+        <div>
+          <button> Add </button>
+        </div>
+      </div>
+      <div className="todo">
+        <div className="list">
+          <div>
+             <p>Read work emails</p>
+          </div>
+          <div>
+             <img src={vector}/>
+          </div>
+        </div>
+        <div className="list">
+          <div>
+             <p className='strike'>Workout</p>
+          </div>
+          <div>
+             <img className='strike' src={vector}/>
+          </div>
+        </div>
+        <div className="list">
+          <div>
+             <p>Water indoor plants</p>
+          </div>
+          <div>
+             <img src={vector}/>
+          </div>
+        </div>
+
+
+      </div>
     </div>
   );
 }
